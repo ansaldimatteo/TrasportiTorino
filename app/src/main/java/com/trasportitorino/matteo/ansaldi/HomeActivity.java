d3b7e66daf3e4d8048e742883fefc1ab7ed17304
ansaldimatteo <ansaldimatteo@yahoo.co.uk>
2017-08-02 17:50:36 +0200

    Saving, deleting functionality added. Now names of bus stops are shown. Changed how the bus numbers are saved in the app (before they were hardcoded, now they are actually saved properly)

--- app/src/main/java/com/trasportitorino/matteo/ansaldi/HomeActivity.java
+++ app/src/main/java/com/trasportitorino/matteo/ansaldi/HomeActivity.java
@@ -3,6 +3,7 @@ package com.trasportitorino.matteo.ansaldi;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.support.v4.widget.DrawerLayout;
@@ -44,12 +45,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private DrawerLayout mDrawerLayout;
     private String mActivityTitle;
 
-
+    private SharedPreferences sharedPref;
 
     private GttBusStop busStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
@@ -60,7 +62,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         btt_search = (ImageButton)findViewById(R.id.btt_search);
         txt_busStop = (EditText)findViewById(R.id.txt_stopNum);
         busStopLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
 
 
         //create the result list
@@ -87,7 +88,10 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         lsv_savedStops.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                txt_busStop.setText(mAdapter.getItem(position));
+                //parse the value read from the list(477 - S. Ottavio --> 477)
+                String text = mAdapter.getItem(position);
+                String stopNumToSearch = text.split(" - ")[0];
+                txt_busStop.setText(stopNumToSearch);
                 btt_search.performClick();
                 busStopLayout.closeDrawers();
             }
@@ -147,11 +151,14 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    //TODO: READ STOP INFO FROM EXTERNAL FILE
     //populate the side drawer
     private void addDrawerItems() {
-        String[] savedStopsArray = { "477","624" };
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, savedStopsArray);
+        ArrayList<String> savedStops = new ArrayList<>();
+        //savedStops.addAll(sharedPref.getAll().keySet());
+        for(String stopNum : sharedPref.getAll().keySet()){
+            savedStops.add(stopNum + " - " + sharedPref.getAll().get(stopNum));
+        }
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, savedStops);
         lsv_savedStops.setAdapter(mAdapter);
     }
 
@@ -258,24 +265,28 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 //TODO: code to add extra stop
 
                 // get prompts.xml view
-                LayoutInflater li = LayoutInflater.from(context);
+                LayoutInflater li = LayoutInflater.from(this);
                 View promptsView = li.inflate(R.layout.add_stop_layout, null);
 
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        context);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 
                 // set prompts.xml to alertdialog builder
                 alertDialogBuilder.setView(promptsView);
 
+                final EditText txt_stopNumToSave = (EditText)promptsView.findViewById(R.id.stopNumberA);
+                final EditText txt_stopNameToSave = (EditText)promptsView.findViewById(R.id.stopNameA);
                 // set dialog message
                 alertDialogBuilder
                         .setCancelable(false)
                         .setPositiveButton("OK",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog,int id) {
-                                        // get user input and set it to result
-                                        // edit text
-                                        //TODO:save data from the alert onto a file containing the stops
+                                        // save the new stop number and stop name
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+                                        editor.putString(txt_stopNumToSave.getText().toString(), txt_stopNameToSave.getText().toString());
+                                        editor.commit();
+                                        addDrawerItems();
+
                                     }
                                 })
                         .setNegativeButton("Cancel",
@@ -296,24 +307,26 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.delete_stop:
 
                 // get prompts.xml view
-                LayoutInflater li_delete = LayoutInflater.from(context);
+                LayoutInflater li_delete = LayoutInflater.from(this);
                 View promptsView_delete = li_delete.inflate(R.layout.delete_stop_layout, null);
 
-                AlertDialog.Builder alertDialogBuilder_delete = new AlertDialog.Builder(
-                        context);
+                AlertDialog.Builder alertDialogBuilder_delete = new AlertDialog.Builder(this);
 
                 // set prompts.xml to alertdialog builder
                 alertDialogBuilder_delete.setView(promptsView_delete);
 
+                final EditText txt_stopNumToDelete = (EditText)promptsView_delete.findViewById(R.id.stopNumberD);
                 // set dialog message
                 alertDialogBuilder_delete
                         .setCancelable(false)
                         .setPositiveButton("OK",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog,int id) {
-                                        // get user input and set it to result
-                                        // edit text
-                                        //TODO:delete data given from the alert
+                                        // delete stop number and name
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+                                        editor.remove(txt_stopNumToDelete.getText().toString());
+                                        editor.commit();
+                                        addDrawerItems();
                                     }
                                 })
                         .setNegativeButton("Cancel",
